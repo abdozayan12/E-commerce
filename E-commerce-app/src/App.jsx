a5a1b9af0347eefd631d761bec9dabd6bd5c48b8
ABDEL HAKEM ZAYAN <abdozayan12@gmail.com>
2025-02-22 21:55:04 +0200

    Update App.jsx

--- E-commerce-app/src/App.jsx
+++ E-commerce-app/src/App.jsx
@@ -3,16 +3,14 @@ import Home from './home';
 import ProductDetail from './ProductDetail';  
 import Cart from './Cart';         
 import { CartProvider } from './CartContext';
-import nav from './nav';
+import Nav from './nav'; // Assuming 'nav' is a component, it should be capitalized
 import './App.css';
-
 
 function App() {
   return (
     <CartProvider>
       <Routes>
-        <Route path="/" element={<nav />} />
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<><Nav /><Home /></>} /> {/* Use Nav component */}
         <Route path="/product/:id" element={<ProductDetail />} />
         <Route path="/cart" element={<Cart />} />
       </Routes>
