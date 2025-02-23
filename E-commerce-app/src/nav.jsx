1e1355a4395a3ac614c8be48c316691511ec1e34
ABDEL HAKEM ZAYAN <abdozayan12@gmail.com>
2025-02-23 20:46:39 +0200

    Update nav.jsx

--- E-commerce-app/src/nav.jsx
+++ E-commerce-app/src/nav.jsx
@@ -1,5 +1,12 @@
 function nav(){
-    
+    return(
+        <nav>
+            <ul>
+                <li><a href="/">Home</a></li>
+                <li><a href="/cart">Cart</a></li>
+            </ul>
+        </nav>
+    );
 }
 
 export default nav;
